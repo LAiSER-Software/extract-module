d27b84cd8dab9ca95dce6a1ae53d02822120bfe8
PrudhvirajuChekuri <prudhvi.chekuri@gwu.edu>
2024-11-26 14:10:36 -0500

    update calls to extractor and add syllabi extraction

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from laiser.skill_extractor import Skill_Extractor
 
+print('\n\nInitializing the Skill Extractor...')
+se = Skill_Extractor() # runs __init__() method
+print('The Skill Extractor has been initialized successfully!\n')
+
+
+# Skill extraction from jobs data
 print('\n\nLoading a sample dataset of 50 jobs...')
 nlx_sample = pd.read_csv('https://raw.githubusercontent.com/LAiSER-Software/datasets/refs/heads/master/jobs-data/nlx_job_data_50rows.csv')
-print('The sample dataset has been loaded successfully!\n')
+print('The sample jobs dataset has been loaded successfully!\n')
 
 
 nlx_sample = nlx_sample[['description', 'job_id']]
@@ -11,14 +17,31 @@ nlx_sample = nlx_sample[1:3]
 print('The sample dataset has been filtered successfully!\n')
 print('Head of the sample:\n', nlx_sample.head())
 
-print('\n\nInitializing the Skill Extractor...')
-se = Skill_Extractor() # runs __init__() method
-print('The Skill Extractor has been initialized successfully!\n')
-
-output = se.extractor(nlx_sample, 'job_id', 'description')
-print('The skills have been extracted successfully...\n')
+output = se.extractor(nlx_sample, 'job_id', text_columns = ['description'])
+print('The skills have been extracted from jobs data successfully...\n')
 
 # save the extracted skills to a csv file
 print(output)
-output.to_csv('./output/test_extracted_skills_for_2Jobs.csv', index=False)
-print('The extracted skills have been saved to the file named: extracted_skills_for_50Jobs.csv')
+file_name = f'extracted_skills_for_{len(nlx_sample)}Jobs.csv'
+output.to_csv(f'./output/{file_name}', index=False)
+print('The extracted skills have been saved to the file named:', file_name)
+
+
+# Skill extraction from syllabi data
+print('\n\nLoading a sample dataset of 50 syllabi...')
+syllabi_sample = pd.read_csv('https://raw.githubusercontent.com/LAiSER-Software/datasets/refs/heads/master/syllabi-data/preprocessed_50_opensyllabus_syllabi_data.csv')
+print('The sample syllabi dataset has been loaded successfully!\n')
+
+syllabi_sample = syllabi_sample[['id', 'description', 'learning_outcomes']]
+syllabi_sample = syllabi_sample[1:3]
+print('The sample dataset has been filtered successfully!\n')
+print('Head of the sample:\n', syllabi_sample.head())
+
+output = se.extractor(syllabi_sample, 'id', text_columns=['description', 'learning_outcomes'], input_type='syllabus')
+print('The skills have been extracted from syllabi data successfully...\n')
+
+# save the extracted skills to a csv file
+print(output)
+file_name = f'extracted_skills_for_{len(syllabi_sample)}Syllabi.csv'
+output.to_csv(f'./output/{file_name}', index=False)
+print('The extracted skills have been saved to the file named:', file_name)
