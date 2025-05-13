5c03565e3fc536f27feccc528885c3ff8d87569e
Satya Phanindra Kumar Kalaga <phanindra.1756.kkr@gmail.com>
2025-05-12 22:47:18 -0400

    Upgrade packaging standards PEP 517, 518, 621, 508, 440, 566

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='laiser',
-    version='0.2.0', 
+    name='dev-laiser',
+    version='0.1.21', 
     author='Satya Phanindra Kumar Kalaga, Prudhvi Chekuri, Bharat Khandelwal', 
     author_email='phanindra.connect@gmail.com',  
     description="LAiSER (Leveraging Artificial Intelligence for Skill Extraction & Research) is a tool designed to help learners, educators, and employers extract and share trusted information about skills. It uses a fine-tuned language model to extract raw skill keywords from text, then aligns them with a predefined taxonomy. You can find more technical details in the project's paper.md and an overview in the README.md.", 
@@ -18,14 +18,14 @@ setup(
     python_requires='>=3.9',
     install_requires=[
         'numpy>=1.21.0,<2.0.0',
-        'pandas==2.2.3',
+        'pandas==2.2.2',
         'psutil>=5.8.0,<6.0.0',
         'skillNer==1.0.3',
         'scikit-learn>=1.0.0,<2.0.0',
         'spacy>=3.0.0,<4.0.0',
         'transformers>=4.0.0,<5.0.0',
         'tokenizers>=0.10.0,<1.0.0',
-        'torch==2.2.2',
+        'torch==2.6.0',
         'ipython>=7.0.0,<8.0.0',
         'python-dotenv>=0.19.0,<1.0.0',
         'vllm>=0.1.0,<1.0.0',
@@ -33,7 +33,7 @@ setup(
     ],
     extras_require={
         'gpu': [
-            'torch==2.2.2',
+            'torch==2.6.0',
             'vllm>=0.1.0,<1.0.0'
         ],
         'cpu': [
