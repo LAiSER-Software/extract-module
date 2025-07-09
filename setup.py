b4e1fb9e42a7580baeb457f8634198848eb524e0
Anket <patilanket11@gmail.com>
2025-07-09 19:23:39 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dev-anket-laiser',
-    version='2.2.16', 
+    version='2.2.17', 
     author='Satya Phanindra Kumar Kalaga, Prudhvi Chekuri, Bharat Khandelwal, Anket Patil', 
     author_email='phanindra.connect@gmail.com',  
     description="LAiSER (Leveraging Artificial Intelligence for Skill Extraction & Research) is a tool designed to help learners, educators, and employers extract and share trusted information about skills. It uses a fine-tuned language model to extract raw skill keywords from text, then aligns them with a predefined taxonomy. You can find more technical details in the project's paper.md and an overview in the README.md.", 
